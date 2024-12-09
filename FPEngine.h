02a53bb710b3a28bab6581cd97b415164d6d92a7
Gavin Garzia <ggarzia@mines.edu>
2024-12-09 15:52:00 -0700

    first person view

--- FPEngine.h
+++ FPEngine.h
@@ -6,7 +6,6 @@
 #include <CSCI441/ModelLoader.hpp>
 #include <CSCI441/OpenGLEngine.hpp>
 #include <CSCI441/ShaderProgram.hpp>
-#include "ArcBall.h"
 #include "Plane.h"
 #include "CollisionDetector.h"
 #include "Ghost.h"
@@ -36,7 +35,7 @@ public:
 
     /// \desc handle any cursor movement events inside the engine
     /// \param currMousePosition the current cursor position
-    void handleCursorPositionEvent(glm::vec2 currMousePosition);
+    void handleCursorPositionEvent(GLFWwindow* window, glm::vec2 currMousePosition);
 
     /// \desc handle any scroll events inside the engine
     /// \param offset the current scroll offset
@@ -89,19 +88,17 @@ private:
     //***************************************************************************
     // Camera Information
 
-    /// \desc the arcball camera in our world
-    ArcBall* _pArcBall;
+    static const GLint WINDOW_HEIGHT = 720;
+    static const GLint WINDOW_WIDTH = 720;
+
     GLfloat _direction;
+    GLfloat _phi;
     glm::vec2 _pos;
 
     //0: normal
     //1: collided moving x
     //2: collided moving z
     GLfloat _lastDir;
-
-
-    //vehicles
-    Plane* _pColtonPlane;
 
     GLfloat WORLD_SIZE_X = 60.0f;
     GLfloat WORLD_SIZE_Y = 60.0f;
