924e6e7938d5d55d66828ff0a3506a2440c8933e
Gavin Garzia <ggarzia@mines.edu>
2024-12-09 16:37:42 -0700

    removed unnecessary comments

--- CollisionDetector.cpp
+++ CollisionDetector.cpp
@@ -8,16 +8,6 @@ bool CollisionDetector::checkCollision(const glm::vec2& playerPos, float playerR
 CollisionObject* CollisionDetector::getCollidedObject(const glm::vec2& playerPos, float playerRadius){
     for(int i = 0; i < _collisionObjects.size(); i++) {
         CollisionObject obj = _collisionObjects[i];
-        // Calculate 2D distance (ignoring height)
-        // float distance = glm::length(
-        //     glm::vec2(obj.position.x, obj.position.z) - playerPos
-        // );
-        
-        // // If distance is less than combined radii, collision occurred
-        // if(distance < (playerRadius + obj.radius)) {
-        //     return &obj;
-        // }
-
         if(playerPos.x < obj.position.x + obj.radius && 
         playerPos.x > obj.position.x - obj.radius &&
         playerPos.y < obj.position.z + obj.radius && 
